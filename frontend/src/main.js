0a577e8f8b19d77ba8f76a33ecaff323ff04b460
Chris Knight <chrisk314@gmail.com>
2020-05-05 12:53:54 +0100

    Avoids globally registering components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,14 +3,9 @@ import {component, register} from 'riot'
 import {Route, Router} from '@riotjs/route'
 
 import App from './shared/app.riot'
-import AppMain from './shared/app-main.riot'
-import AppNavi from './shared/app-navi.riot'
 
 register('router', Router)
 register('route', Route)
-
-register('app-main', AppMain)
-register('app-navi', AppNavi)
 
 const mountApp = component(App)
 const app = mountApp(document.getElementById('app'), {})
