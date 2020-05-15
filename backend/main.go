e96c033bcd5aa52ba5e4be6a669aa652d958b280
Chris Knight <chrisk314@gmail.com>
2020-05-15 17:27:24 +0100

    Adds /api/v1 router group

--- backend/main.go
+++ backend/main.go
@@ -12,9 +12,13 @@ func main() {
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
-	})
+	api := router.Group("/api/v1")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"data": "hello world"})
+		})
+	}
+
 	router.NoRoute(func(c *gin.Context) {
 		c.JSON(http.StatusNotFound, gin.H{"message": "Page not found."})
 	})
