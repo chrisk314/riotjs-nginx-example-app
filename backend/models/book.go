48e298466314ce65fe20195176a8b06f3e4b70e9
Chris Knight <chrisk314@gmail.com>
2020-05-17 12:09:36 +0100

    Adds missing publication_date and publisher fields

--- backend/models/book.go
+++ backend/models/book.go
@@ -1,15 +1,21 @@
 package models
 
+import (
+	"time"
+)
+
 // Book models database Book table. Represents a book.
 type Book struct {
-	ID            uint   `json:"id" gorm:"primary_key"`
-	Title         string `json:"title"`
-	Authors       string `json:"authors"`
-	AverageRating string `json:"average_rating"`
-	ISBN          string `json:"isbn"`
-	ISBN13        string `json:"isbn_13"`
-	LanguageCode  string `json:"language_code"`
-	NumPages      int    `json:"num_pages"`
-	Ratings       int    `json:"ratings"`
-	Reviews       int    `json:"reviews"`
+	ID              uint      `json:"id" gorm:"primary_key"`
+	Title           string    `json:"title"`
+	Authors         string    `json:"authors"`
+	AverageRating   string    `json:"average_rating"`
+	ISBN            string    `json:"isbn"`
+	ISBN13          string    `json:"isbn_13"`
+	LanguageCode    string    `json:"language_code"`
+	NumPages        int       `json:"num_pages"`
+	Ratings         int       `json:"ratings"`
+	Reviews         int       `json:"reviews"`
+	PublicationDate time.Time `json:"date"`
+	Publisher       string    `json:"publisher"`
 }
